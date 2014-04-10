2e11f9ecd72ed2a4eab728e2be310b4541b132ce
emreiser <emilie.reiser@gmail.com>
2014-04-10 11:26:13 -0400

    Fixed Firefox event bug for nav links. Modified body fonts.

--- main.js
+++ main.js
@@ -1,49 +1,49 @@
 $(document).ready(function(){
-	$(".project").on("mouseenter", ER.hoverProject);
-	$(".project").on("mouseleave", ER.unhoverProject);
+  $(".project").on("mouseenter", ER.hoverProject);
+  $(".project").on("mouseleave", ER.unhoverProject);
 
-	ER.expanded = false;
-	$(window).on("scroll", ER.expandNav);
-	$('#navbar').click(ER.scrollTo);
+  ER.expanded = false;
+  $(window).on("scroll", ER.expandNav);
+  $('#navbar').click(ER.scrollTo);
 
-	$('#projects').click(ER.visitLink);
+  $('#projects').click(ER.visitLink);
 
 
 });
 
 ER = {
-	about: 538,
-	projects: 0,
-	contact: 1080
+  about: 538,
+  projects: 0,
+  contact: 1080
 };
 
 ER.hoverProject = function(event){
-	$(event.target).addClass("project-spotlight");
-	$(event.target).children(".overlay").animate({opacity: 100}, 200);
+  $(event.target).addClass("project-spotlight");
+  $(event.target).children(".overlay").animate({opacity: 100}, 200);
 };
 
 ER.unhoverProject = function(event){
-	$(event.target).removeClass("project-spotlight");
-	$(event.target).children(".overlay").animate({opacity: 0}, 200);
+  $(event.target).removeClass("project-spotlight");
+  $(event.target).children(".overlay").animate({opacity: 0}, 200);
 };
 
 ER.expandNav = function(){
-	var offset = $(window).scrollTop();
-	if(offset > 215 && ER.expanded == false){
-		$("#nav-top").animate({height: "70px"});
-		ER.expanded = true;
-	} else if(offset < 215 && ER.expanded == true) {
-		$("#nav-top").animate({height: "20px"});
-		ER.expanded = false;
-	}
+  var offset = $(window).scrollTop();
+  if(offset > 215 && ER.expanded == false){
+    $("#nav-top").animate({height: "70px"});
+    ER.expanded = true;
+  } else if(offset < 215 && ER.expanded == true) {
+    $("#nav-top").animate({height: "20px"});
+    ER.expanded = false;
+  }
 };
 
-ER.scrollTo = function(){
-	$('html body').animate({
-		scrollTop: ER[$(event.target).attr("data-section")]
-	}, 800, 'swing');
+ER.scrollTo = function(event){
+  $('html, body').animate({
+    scrollTop: ER[$(event.target).attr("data-section")]
+  }, 800, 'swing');
 };
 
 ER.visitLink = function(){
-	$project.removeClass("project-spotlight");
+  $project.removeClass("project-spotlight");
 }
