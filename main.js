7c9c83bebc765eb30e8ededb270f72afe160958d
emreiser <emilie.reiser@gmail.com>
2014-05-19 18:33:02 -0400

    Add project descriptions

--- main.js
+++ main.js
@@ -7,9 +7,19 @@ $(document).ready(function(){
   $(window).on("scroll", ER.toggleClouds);
   $("#navbar li, p").click(ER.scrollTo);
   $("#initials").on("mouseenter", ER.setOn).on("mouseleave", ER.setOff);
+  $(".project").on("click", function(event){
+    ER.openDesc(event);
+    ER.scrollTo(event);
+  });
+  $(document).on("click", ".close-project", ER.closeDesc);
+
+  $(document).on("click", ".next", function(event){
+    ER.selected = ER.getNext(ER.projects, ER.selected);
+    ER.setProject(event, ER.selected);
+  });
 });
 
-var ER = {};
+var ER = ER || {};
 
 ER.getElem = function(id){
   if (!ER.getElem.cache) { ER.getElem.cache = {}; }
@@ -46,7 +56,7 @@ ER.setCollapse = function(event){
 ER.toggleClouds = function(event){
   var contactTop, yPos, diff;
   contactTop = $('#contact').position().top;
-  yPos = $(window).scrollTop() + $(window).height() - 175;
+  yPos = $(window).scrollTop() + $(window).height() - 200;
   diff = yPos - contactTop;
 
   if(diff < 0){
@@ -76,5 +86,56 @@ ER.floatClouds = function(event){
 
 ER.enterNav = function(){
   $('#navbar').animate({top: 0}, 400);
+};
 
+ER.openDesc = function(event){
+  ER.setProject(event);
+  $(ER.getElem('project-description')).slideDown();
+};
+
+ER.closeDesc = function(){
+  $(ER.getElem('project-description')).slideUp();
+};
+
+ER.setProject = function(event, name){
+  ER.selected = name || event.target.id;
+  var project = ER.projects[ER.selected];
+  $('#project-description .title').text(project.title);
+  $('#project-description .tag').html(project.tag.replace(/\n/g, "<br>"));
+  $('#project-description .desc').html(project.description.replace(/\n/g, "<br>"));
+  $('#project-description .tech').html(project.tech);
+  $('#project-description img').attr("src", (project.image));
+  $('#project-description a').attr("href", (project.link));
+};
+
+ER.getNext = function(object, currentKey){
+  var keys = Object.keys(object), index = keys.indexOf(currentKey);
+  return keys[index + 1] ? keys[index + 1] : keys[0]  ;
+};
+
+ER.projects = {
+  "go-figure": {
+    title: "Go Figure",
+    tag: "A Comparative Look at Countries through Human Development Statistics",
+    description: "Go Figure was designed to explore our perceptions of how countries stack up through the lens of the Human Development Report indicators. \nThe site engages viewers through a series of questions comparing countries' ranks in several key areas from gender equality to innovation and tracks bias through their responses.",
+    link: "http://go-figure.herokuapp.com",
+    tech: "RUBY | RAILS | HAML | HANDLEBARS | COFFEESCRIPT | D3",
+    image: "//s3.amazonaws.com/emiliereiser.com/go-figure-2-small.jpg"
+  },
+  loopd: {
+    title: "Loopd",
+    tag: "A Simple RSS Feed Aggregator",
+    description: "Loopd is a light-weight RSS reader that allows you to bring together your favorite content on the web in one place. Feeds can be tagged, categorized and filtered",
+    link: "http://loopdin.herokuapp.com/",
+    tech: "RUBY | RAILS | JAVASCRIPT | JQUERY | RSPEC | CAPYBARA",
+    image: "//s3.amazonaws.com/emiliereiser.com/loopd-2-small.jpg"
+  },
+  outflux: {
+    title: "Outflux",
+    tag: "An Interactive Map of International Refugee Flows",
+    description: "Every year, millions of people around the world are forced to flee their countries of origin due to violence, persecution and natural disaster. \nOutflux was created with historical data from the UN Refugee Agency and was designed to help users visualize ongoing global refugee crises and access specific country information from UNHCR's news feed.",
+    link: "http://outflux.herokuapp.com/",
+    tech: "RUBY | RAILS | DEVISE | UNDP API",
+    image: "//s3.amazonaws.com/emiliereiser.com/outflux-2-small.jpg"
+  }
 };
